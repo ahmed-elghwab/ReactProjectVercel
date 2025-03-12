8d7ad057147e57db624a3262ea73b24e7299fc43
ahmed-elghwab <ahmed.elghwab224@gmail.com>
2025-03-12 11:49:45 +0200

    Fix: Correct Cart import path3

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import CheckOut from './components/CheckOut/CheckOut'
 import AllOrders from './components/AllOrders/AllOrders'
 import PasswordReset from './components/PasswordReset/PasswordReset'
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import Cart from './components/cart/cart.jsx';
+import Cart from '@/components/cart/cart';
 
 
 
